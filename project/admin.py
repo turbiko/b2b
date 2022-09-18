babc671aa3e05c6a9c3c5b883bf5667f775c6208
Andriy Voznyuk <andreyv@ukr.net>
2022-09-18 13:42:58 +0200

    Update admin.py

--- project/admin.py
+++ project/admin.py
@@ -5,5 +5,10 @@ admin.site.register(Genre)
 admin.site.register(ProjectGenres)
 admin.site.register(Project)
 admin.site.register(FileFolder)
-admin.site.register(FileInFolder)
+# admin.site.register(FileInFolder)
 admin.site.register(NewsArticle)
+
+
+@admin.register(FileInFolder)
+class FileInFolderAdmin(admin.ModelAdmin):
+    list_display = ("name", "page")
