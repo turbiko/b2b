ef1e16b0b00fd700d6d11c2a627a6434fa5300ff
Andriy Voznyuk <andreyv@ukr.net>
2022-09-18 13:01:33 +0200

    Update admin.py

--- project/admin.py
+++ project/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Genre
+from .models import Genre, ProjectGenres, Project, Projects, FileFolder, FileInFolder, NewsArticle
 
 admin.site.register(Genre)
+admin.site.register(ProjectGenres)
+admin.site.register(Project)
+admin.site.register(FileFolder)
+admin.site.register(FileInFolder)
+admin.site.register(NewsArticle)
