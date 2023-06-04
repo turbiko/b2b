8a9d9f38250c1016e02e279561610e5fa44e8f90
Andriy Voznyuk <andreyv@ukr.net>
2023-06-04 02:00:26 +0200

    Update sendchanges.py
    fix errors
    when group not created
    when no emails to send
    when no changes in project
    change logging messages

--- project/management/commands/sendchanges.py
+++ project/management/commands/sendchanges.py
@@ -28,38 +28,32 @@ class Command(BaseCommand):
 		for group1 in Group.objects.all():  # getting all projects groups to list
 			if group1.name not in ('Moderators', 'Editors'):
 				prj_groups.append(group1.name)
-
+		# DEBUG, INFO, WARNING, ERROR, CRITICAL
 		self.stdout.write(self.style.WARNING(prj_groups))
 
 		## get projects for mailing
 		active_projects = Page.objects.exact_type(Project).live()
 		mail_content = {}
-		mail_to = {}
+		# mail_to = {}
 		for project in active_projects:
 			mail_content[project.slug] = self.select_last_topics(project)
 			print(f'mail_content:{project.slug=} \n', mail_content)
 			emails = self.get_subscriber_emails(project)
-
-			# send_mail(
-			# subject,
-			# message,
-			# from_email,
-			# recipient_list,
-			# fail_silently=False, auth_user=None, auth_password=None, connection=None, html_message=None)
-			try:
-				self.stdout.write(self.style.WARNING(f"About to send email to {emails}"))
-				# Logic to send email here
-				for email_to_send in emails:
-					send_mail(
-							f'B2B [{project.title}] updates digest {datetime.now().date().strftime("%d %B, %Y")}',
-							mail_content[project.slug].replace(u'\xa0', u' '),
-							settings.EMAIL_HOST_USER,
-							[email_to_send],
-							fail_silently=False,
-					)
-				self.stdout.write(self.style.WARNING("After successful sent email "))
-			except Exception as ex:
-				raise CommandError(f'Failed to send test email to {emails=}  {ex}')
+			if emails and mail_content[project.slug]:
+				try:
+					self.stdout.write(self.style.WARNING(f"About to send email to {emails}"))
+					# Logic to send email here
+					for email_to_send in emails:
+						send_mail(
+								f'B2B [{project.title}] updates digest {datetime.now().date().strftime("%d %B, %Y")}',
+								mail_content[project.slug].replace(u'\xa0', u' '),
+								settings.EMAIL_HOST_USER,
+								[email_to_send],
+								fail_silently=False,
+						)
+					self.stdout.write(self.style.INFO("After successful sent email "))
+				except Exception as ex:
+					raise CommandError(f'Failed to send email to {emails=} \n {ex}')
 
 
 	def select_last_topics(self, project_page, days=7) -> str:
@@ -72,24 +66,31 @@ class Command(BaseCommand):
 		content = ''
 		d0 = date.today()
 		d1 = d0 - timedelta(days=days)
-		print(d1, d0)
+		print(f'today: {d1}  time-delta: {d0} \n days: {days}')
 		pages_for_report = pages.filter(last_published_at__gte=d1)
 		for p in pages_for_report:
 			# print(p.last_published_at.date(), d0, d1)
 			content += str(timesince(p.last_published_at).split(',')[0]).replace(u'\xa0', u' ')
 			content += f' {p.title}  {p.url}  \n '
 		if content == '':
-			self.stdout.write(self.style.ERROR(f'no changes since {str(d1)}'))
+			self.stdout.write(self.style.WARNING(f'no changes since {str(d1)}'))
 		print(f'content ({project_page.title}): \n', content)
 		return content
 
 
 	def get_subscriber_emails(self, project_page:Project) -> list:
-		self.stdout.write(self.style.WARNING(f'Create emails list for {project_page.title=}'))
-		selected_group = Group.objects.get(name=project_page.slug)
+		self.stdout.write(self.style.INFO(f'Create emails list for {project_page.title=}'))
+		try:
+			selected_group = Group.objects.get(name=project_page.slug)
+		except Group.DoesNotExist:
+			self.stdout.write(self.style.WARNING(f'Group does not exist. Creating new group {project_page.slug} '))
+			selected_group = Group.objects.create(name=project_page.slug)
 		project_users = selected_group.user_set.all()  # users in group
 		emails_to_send = []
-		for u in project_users:
-			emails_to_send.append(u.email)
-		self.stdout.write(self.style.WARNING(f'Created emails list for {emails_to_send=}'))
+		if project_users:
+			for u in project_users:
+				emails_to_send.append(u.email)
+			self.stdout.write(self.style.INFO(f'Created emails list for {project_page.title=}'))
+		else:
+			self.stdout.write(self.style.WARNING(f'No users in the group {project_page.slug}. Skip creating users list.'))
 		return emails_to_send
