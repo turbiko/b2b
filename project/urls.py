d06a511b65d3188468a8562ededd6eefd3bb51b0
Andriy Voznyuk <andreyv@ukr.net>
2023-06-20 11:35:26 +0200

    fixes 1
    year on plnned and delete from url test path and comments

--- project/urls.py
+++ project/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	# path('uploadprojectfiles/<str:pk>/', views.addPhoto, name='uploads'),
 	path('uploadprojectfiles/<str:pk>/', views.add_photo, name='uploads'),
-	path('planned/', views.projects_planned, name='projects_planned'),
-
 ]
 
