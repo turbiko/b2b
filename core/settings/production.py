58a483db4ed89f49cfaeabe5b49eabe4132aef7c
Andriy Voznyuk <andreyv@ukr.net>
2023-07-13 19:09:24 +0200

    Update production.py

--- core/settings/production.py
+++ core/settings/production.py
@@ -4,12 +4,6 @@ from .base import *  # noqa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 print('DEBUG.prod= ', DEBUG)
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATIC_URL = "/static/"
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "dm*#_9rs0r0z$g)65#m12y6wxl@rpw$%1dxu@+(x^dm*#_9rs0r0z$g)65#m12y6wv!=2b%"
 
 DATABASES = {
     "default": {
@@ -23,19 +17,18 @@ DEFAULT_FROM_EMAIL = "b2b.film.ua <noreply@argentum.ua>"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 CSRF_TRUSTED_ORIGINS = [
-    'https://b2b.argentum.ua',
     'https://b2b.film.ua',
 ]
 
 # A list of people who get error notifications.
-ADMINS = [('admin', 'andreyv@ukr.net'), ('admin2', 'avoznyuk@film.ua')]
+ADMINS = [('Developer', 'a.voznyuk@film.ua')]
 
 # A list in the same format as ADMINS that specifies who should get broken link
 # (404) notifications when BrokenLinkEmailsMiddleware is enabled.
 MANAGERS = ADMINS
 
 # Email address used to send error messages to ADMINS.
-SERVER_EMAIL = "Error " + DEFAULT_FROM_EMAIL
+SERVER_EMAIL = f"Error {DEFAULT_FROM_EMAIL}"
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = [
